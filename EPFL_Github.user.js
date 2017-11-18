c83bc983fa0814156bc8f7ee1f232941eff1942c
ponsfrilus <ponsfrilus@gmail.com>
2017-11-18 13:49:30 +0100

    v0.0.2 - new icons

--- EPFL_Github.user.js
+++ EPFL_Github.user.js
@@ -6,7 +6,7 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @author       ponsfrilus
 // @downloadURL  https://raw.githubusercontent.com/ponsfrilus/EPFL_Github_UserScript/master/EPFL_Github.user.js
-// @version      0.0.1
+// @version      0.0.2
 // ==/UserScript==
 
 // Avoid conflicts
@@ -15,10 +15,10 @@ $(document).ready( function() {
   console.log("... EPFL Github User script loaded ...");
 
   // Insert a link to EPFL STI-IT
-  $( '.header-logo-invertocat' ).before( '<a class="header-logo-epfl-sti-it" href="https://github.com/epfl-sti/"><img height="32" width="32" src="https://raw.githubusercontent.com/epfl-sti/sti-it-design/master/r_sti-it.png" /></a>&nbsp;' );
+  $( '.header-logo-invertocat' ).before( '<a class="header-logo-epfl-sti-it" href="https://github.com/epfl-sti/"><img height="32" width="32" src="https://raw.githubusercontent.com/ponsfrilus/EPFL_Github_UserScript/master/img/epfl-stiit.png" /></a>&nbsp;' );
 
   // Insert a link to EPFL-Dojo
-  $( '.header-logo-invertocat' ).before( '<a class="header-logo-epfl-dojo" href="https://github.com/epfl-dojo/"><img height="32" width="32" src="https://raw.githubusercontent.com/epfl-dojo/dojo-design/master/dojoman/dojoman_circle.png" /></a>&nbsp;' );
+  $( '.header-logo-invertocat' ).before( '<a class="header-logo-epfl-dojo" href="https://github.com/epfl-dojo/"><img height="32" width="32" src="https://raw.githubusercontent.com/ponsfrilus/EPFL_Github_UserScript/master/img/epfl-dojo.png" /></a>&nbsp;' );
 
   // Insert a link to EPFL's topic
   $( '.header-logo-invertocat' ).before( '<a class="header-epfl-topic" href="https://github.com/topics/epfl"><img height="32" width="32" src="https://raw.githubusercontent.com/ponsfrilus/EPFL_Github_UserScript/master/img/epfl-topic.png" /></a>&nbsp;' );
